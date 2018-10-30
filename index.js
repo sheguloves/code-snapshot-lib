c0628d5840bd4f99fc51379042b656d7cc7398bd
Guofeng Xi <guofeng.xi@nio.com>
2018-10-30 11:35:49 +0800

    adjust file path to fit github

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({
 
 app.use('/upload', express.static(path.join(__dirname, './upload')))
 app.use('/assets', express.static(path.join(__dirname, './assets')))
-app.use('/pages', express.static(path.join(__dirname, './static')))
+app.use('/static', express.static(path.join(__dirname, './static')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
